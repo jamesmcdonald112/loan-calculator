e7fbdf4569347f1e38c8c568be6a44d82936adc8
James McDonald <jamesmcdonald112@yahoo.ie>
2025-01-16 13:05:12 +0000

    Stage 1 complete

--- Loan Calculator/task/creditcalc/creditcalc.py
+++ Loan Calculator/task/creditcalc/creditcalc.py
@@ -2,3 +2,7 @@
 principal_loan = int(input("Enter the loan principal:"))
 
 monthly_repayment = int(input("Enter the monthly payment:"))
+
+time_in_months_to_repay = principal_loan // monthly_repayment;
+
+print(f"It will take {time_in_months_to_repay} months to repay the loan")
